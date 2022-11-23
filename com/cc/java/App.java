d09b00ca5ffa8e30cb646750051896bf5099b93d
Hindax <haar162@more2learn.de>
2022-11-23 13:05:13 +0100

    konto1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,24 @@
 package com.cc.java;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Konto konto1 = new Konto(0);
+
+
+
+        // output(konto1.getKontoStand()); 
+    
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
 
-
+   
 
 }
 
