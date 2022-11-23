1417120adf195d1211483c5aced2a50d735d0f49
Hindax <hind.aarif@gmail.com>
2022-11-23 18:38:25 +0100

    solution

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,18 +4,39 @@ public class App {
     
     public static void main(String[] args) {
 
-        // int kontoStand = 10000;
-
-        Konto konto1 = new Konto("Konto2");
-        output(konto1.getName());
-
-        // output(String.valueOf(konto1.getKontoStand()));
-        // konto1.setKontoStand(10000);
+        int kontoStand = 10000;
+        // int neukontoStand = (kontoStand + 2);
         
-        // output(konto1.getKontoStand());
-        // System.out.println("Kontostand ist: " );
+        Konto konto1 = new Konto("Konto1", kontoStand, (kontoStand * 2));
+        output(konto1.getName());
+        output(String.valueOf(konto1.getKontoStand()));
+        output(String.valueOf(konto1.getNeuKontoStand()));
 
-        // Konto konto2 = new Konto(10000);
+        output("Erster Konto ist :" + konto1.getName() + ". Der Kontostand war: "
+        + konto1.getKontoStand() + ". Der neuer Kontostand ist " + konto1.getNeuKontoStand() + ".") ;
+
+
+        output("-------------------------------");
+
+        Konto konto2 = new Konto("Konto2", kontoStand , (kontoStand * 3));
+        output(konto2.getName());
+        output(String.valueOf(konto2.getKontoStand()));
+        output(String.valueOf(konto2.getNeuKontoStand()));
+
+        output("Zweiter Konto ist :" + konto2.getName() + ". Der Kontostand war: "
+         + konto2.getKontoStand() + ". Der neuer Kontostand ist " + konto2.getNeuKontoStand()) ;
+
+        output("-------------------------------");
+
+        Konto konto3 = new Konto("Konto3", kontoStand, (kontoStand * 10));
+        output(konto3.getName());
+        output(String.valueOf(konto3.getKontoStand()));
+        output(String.valueOf(konto3.getNeuKontoStand()));
+
+        output("Dritter Konto ist :" + konto3.getName() + ". Der Kontostand war: "
+        + konto3.getKontoStand() + ". Der neuer Kontostand ist " + konto3.getNeuKontoStand() + ".");
+
+
 
     }
 
