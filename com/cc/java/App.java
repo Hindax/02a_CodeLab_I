dd171cfbedaed1b4c72c6580ae8340d5b9cbabfb
Hindax <hind.aarif@gmail.com>
2022-11-23 17:26:29 +0100

    test1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,24 +1,29 @@
 package com.cc.java;
 
-import javax.print.attribute.standard.OutputDeviceAssigned;
-
 public class App {
     
     public static void main(String[] args) {
 
-        Konto konto1 = new Konto(0);
+        // int kontoStand = 10000;
 
+        Konto konto1 = new Konto("Konto2");
+        output(konto1.getName());
 
+        // output(String.valueOf(konto1.getKontoStand()));
+        // konto1.setKontoStand(10000);
+        
+        // output(konto1.getKontoStand());
+        // System.out.println("Kontostand ist: " );
 
-        // output(konto1.getKontoStand()); 
-    
+        // Konto konto2 = new Konto(10000);
+
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
+}
 
    
 
-}
 
