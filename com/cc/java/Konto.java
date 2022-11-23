f465e19163bb4396bd576e7a220166d29eea11f4
Hindax <hind.aarif@gmail.com>
2022-11-23 18:38:37 +0100

    solution with get/set

--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -4,77 +4,51 @@ package com.cc.java;
 
 public class Konto {
 
-//   private int kontoStand;
-  private String name;
+    private String name;
+    private int kontoStand;
+    private int neuKontoStand;
 
-    public Konto(String name) {
+    public Konto(String name, int kontoStand, int neuKontoStand) {
         this.name = name;
-        
-    }
+        this.kontoStand = kontoStand;
+        this.neuKontoStand = neuKontoStand;
+        }
 
-
-//   public Konto(int kontoStand) {
-//     this.kontoStand = kontoStand;
-  
 
     public String getName() {
         return name;
-    
-
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+
+    public int getKontoStand() {
+        return kontoStand;
+    }
+
+
+    public int getNeuKontoStand() {
+        return neuKontoStand;
+    }
+
+
+    public void setKontoStand(int kontoStand) {
+        this.kontoStand = kontoStand;
+    }
+
+
+    public void setNeuKontoStand(int neuKontoStand) {
+        this.neuKontoStand = neuKontoStand;
+    }
+
+    
     
 
 
 }
     
 
-//   Konto(int kontoStand) {
-//     this.kontoStand = kontoStand;
-//}
 
-// public int getKontoStand() {
-//     return kontoStand;
-// }
-
-// public void setKontoStand(int kontoStand) {
-//     this.kontoStand = kontoStand;
-// }
-
-// }
-
-// }
-// Konto konto1 = new Konto(kontoStand);   
-
-// public Konto getKonto1() {
-//     return konto1;
-// }
-
-// public void setKonto1(Konto konto1) {
-//     this.konto1 = konto1;
-// }
-
-// Konto konto2 = new Konto(kontoStand);
-
-
-// public Konto getKonto2() {
-//     return konto2;
-// }
-
-// public Konto getKonto3() {
-//     return konto3;
-// }
-// Konto konto3 = new Konto(kontoStand);
-
-// public void setKonto2(Konto konto2) {
-//     this.konto2 = konto2;
-// }
-
-// public void setKonto3(Konto konto3) {
-//     this.konto3 = konto3;
-// }
 
